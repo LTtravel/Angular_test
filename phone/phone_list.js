617eaea5a527b6211d79621134c7f2d2ba1e7084
LTtravel <1359781852@qq.com>
2016-11-02 18:18:20 +0800

    name
    Name of phone

--- phone/phone_list.js
+++ phone/phone_list.js
@@ -1,8 +1,28 @@
 var cartsController = function($scope){
 	$scope.cart = [
-		{},
-		{},
-		{},
-		{}
+		{
+			id:2099,
+			name:'vivo y18l',
+			quantity:'3',
+			price:'1290'
+		},
+		{
+			id:1523,
+			name:'honmi',
+			quantity:'21',
+			price:'1099'
+		},
+		{
+			id:1621,
+			name:'Hisin',
+			quantity:'8',
+			price:'800'
+		},
+		{
+			id:2148,
+			name:'Nokiio',
+			quantity:'10',
+			price:'599'
+		}
 	];
 }
